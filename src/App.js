5f702846bf91ab2e90bd7828c85be15a18fede9f
Gawain B. II <bracy.gawain2014@gmail.com>
2021-01-21 22:14:30 -0500

    initial push to repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './App.css';
+import Layout from './components/Shared/Layout';
+import Login from './components/Authentication/login';
+import SignUp from './components/Authentication/signup';
+import ForgotPassword from './components/Authentication/forgotpassword';
+import NotFound from './components/Authentication/404';
+import Maintenance from './components/Authentication/maintenance';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	render() {
+		const { themeColor, fontStyle, lightVersion, RtlVersion, offcanvas, miniSidebar, horizontalMenu, miniHover } = this.props
+		document.getElementsByTagName('body')[0].className = `${themeColor} ${fontStyle}${lightVersion ? ' light_version' : ''}${RtlVersion ? ' rtl' : ''}${offcanvas ? ' offcanvas-active' : ''}${horizontalMenu ? ' h-menu' : ''}${miniSidebar ? ' mini_sidebar' : ''}${miniHover ? ' mini_hover' : ''}`;
+		return (
+			<div
+				ref={leftSidebar => {
+					this.leftSidebar = leftSidebar;
+				}}
+			>
+				<Router>
+					<Switch>
+						<Route path="/signup" component={SignUp} />
+						<Route path="/login" component={Login} />
+						<Route path="/forgotpassword" component={ForgotPassword} />
+						<Route path="/notfound" component={NotFound} />
+						<Route path="/maintenance" component={Maintenance} />
+						<Route component={Layout} />
+					</Switch>
+				</Router>
+			</div>
+		);
+	}
 }
 
-export default App;
+// const mapStateToProps = state => ({
+// 	themeColor: state.settings.themeColor,
+// 	fontStyle: state.settings.fontStyle,
+// 	lightVersion: state.settings.lightVersion,
+// 	RtlVersion: state.settings.RtlVersion,
+// 	offcanvas: state.settings.offcanvas,
+// 	horizontalMenu: state.settings.horizontalMenu,
+// 	miniSidebar: state.settings.miniSidebar,
+// 	miniHover: state.settings.miniHover,
+// })
+
+// const mapDispatchToProps = dispatch => ({})
+
+// export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default connect()(App)
